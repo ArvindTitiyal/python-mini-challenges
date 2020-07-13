f5e38a1e5557218cfc4f3ff250cadbee92d8541e
ArvindTitiyal <ArvindTitiyal@users.noreply.github.com>
2020-07-13 21:24:15 +0530

    Code file created.

---
+++ code.py
@@ -0,0 +1,94 @@
+# --------------
+#Code starts here
+
+#Function to check for palindrome
+def palindrome_check(num):
+  num=str(num)
+  return (num[::-1]==num)
+
+#Function to find the smallest palindrome
+def palindrome(num):
+    while(1):
+        num=num+1
+        if palindrome_check(num):
+            return num
+        
+        
+#Code ends here     
+
+
+# --------------
+#Code starts here
+
+#Function to find anagram of one word in another
+
+def a_scramble(str_1,str_2):
+    result=True
+    for i in (str_2.lower()):
+        if i not in (str_1.lower()):
+            result=False
+            break
+        str_1=str_1.replace(i,'',1) #Removing the letters from str_1 that are already checked
+    
+    return (result)
+
+#Code ends here
+
+
+# --------------
+#Code starts here
+import math
+
+# function to check perferct square
+def checkPerfectSquare(n):
+    sqrt = int(math.sqrt(n))
+    if pow(sqrt, 2) == n:
+        return True
+    else:
+        return False
+        
+def check_fib(num):
+    res1 = 5 * num * num + 4
+    res2 = 5 * num * num - 4
+    if checkPerfectSquare(res1) or checkPerfectSquare(res2):
+        return True
+    else:
+        return False
+
+    
+
+
+
+
+# --------------
+#Code starts here
+
+#Function to compress string
+def compress(word):
+    word=word.lower()
+    mist=[]
+    l=0
+    while(l<len(word)):
+        m=word[l]
+        j=0
+        while(l<len(word) and word[l]==m):
+                 j=j+1
+                 l=l+1    
+
+        mist.append(m)
+        mist.append(str(j))
+    
+    return ''.join(mist)
+
+#Code ends here
+    
+
+
+
+# --------------
+#Code starts here
+def k_distinct(string,k):
+    str1 = set(string.lower())
+    return len(str1)==k
+
+
